963ef5d158540e04037b861499af19b79942f0bd
Monika Williams <monika.m.williams@gmail.com>
2017-12-17 09:23:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const app = "I don't do much.";
-fetch('https://api.github.com/repos/jquery/jquery/commits')
+
+
+  fetch('https://api.github.com/user/repos', {
+  	headers: {
+  		Authorization: `token ${token}`
+  	}
+  })
   .then(res => res.json())
   .then(json => console.log(json));
